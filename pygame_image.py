6610816014ff5781850d06a0ad52029e9d1bc426
ibuki kudo <coc24023@edu.teu.ac.jp>
2025-11-18 17:21:44 +0900

    練習１０のコミット

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,10 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     bg_img2 = pg.transform.flip(bg_img,True,False)
     kk_img = pg.transform.flip(kk_img,True,False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+
+
 
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
     tmr = 0
@@ -26,7 +30,20 @@ def main():
         screen.blit(bg_img,[-x,0])
         screen.blit(bg_img2,[-x+1600,0])
         screen.blit(bg_img,[-x+3200,0])
-        screen.blit(kk_img,[300,200])
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, +1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1, 0))
+        screen.blit(kk_img,kk_rct)
+
+
+        # screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
